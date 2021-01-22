ce20f5fd4b98faef1cf25a7d0fa46662e1457707
Hautham <alagarajah.hautham@gmail.fr>
2021-01-22 12:27:46 +0100

    Commit Model complété

---
+++ Projet2/Projet2/Models/Authentification.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Projet2.Models
+{
+    public class Authentification
+    {
+
+
+        public finConnexion()
+        {
+
+        }
+
+        public pageConnexion()
+        {
+
+        }
+    }
+}
---
+++ Projet2/Projet2/Models/Home.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Projet2.Models
+{
+    public class Home
+    {
+
+
+        public pageAccueil()
+        {
+
+        }
+    }
+}
